afa7e52cbf6a8747934134fa91d04b101af35178
Olga Kozlovich <eexmellie@gmail.com>
2019-02-26 00:48:00 +0300

    feat: add love triangles count

--- src/index.js
+++ src/index.js
@@ -3,5 +3,31 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  for (let i = 0; i < preferences.length; i++) {
+
+    // skipping valid checked triangle
+    if (preferences[i] === -1) continue;
+    
+    const firstLover = preferences[i];
+    const secondLover = preferences[firstLover - 1];
+    const thirdLover = preferences[secondLover - 1];
+
+    // checking wether the lovers form a triangle
+    const isTriangleValid = thirdLover - 1 === i;
+
+    // checking that lovers aren't one "person"
+    const areLoversUnique = firstLover !== secondLover;
+
+    if (isTriangleValid && areLoversUnique) {
+      count++;
+
+      // marking lovers of valid triangle as checked
+      preferences[i] = -1;
+      preferences[firstLover - 1] = -1;
+      preferences[secondLover - 1] = -1;
+    }
+  }
+
+  return count;
 };
