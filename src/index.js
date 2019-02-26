f7514e8dc14e08bac9e860d22445fa6ffb4ac41d
Volha Kazlovich <eexmellie@gmail.com>
2019-02-27 01:07:54 +0300

    feat: create Set of indexes of counted triangles

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   let count = 0;
+  const validTriangleIndexes = new Set();
+
   for (let i = 0; i < preferences.length; i++) {
 
-    // skipping valid checked triangle
-    if (preferences[i] === -1) continue;
-    
+    // skipping valid counted lover
+    if (validTriangleIndexes.has(i)) continue;
+
     const firstLover = preferences[i];
     const secondLover = preferences[firstLover - 1];
     const thirdLover = preferences[secondLover - 1];
-
+    
     // checking wether the lovers form a triangle
     const isTriangleValid = thirdLover - 1 === i;
 
@@ -21,13 +23,11 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
 
     if (isTriangleValid && areLoversUnique) {
       count++;
-
-      // marking lovers of valid triangle as checked
-      preferences[i] = -1;
-      preferences[firstLover - 1] = -1;
-      preferences[secondLover - 1] = -1;
+      validTriangleIndexes.add(i);
+      validTriangleIndexes.add(firstLover - 1);
+      validTriangleIndexes.add(secondLover - 1);
     }
   }
-
+  
   return count;
 };
